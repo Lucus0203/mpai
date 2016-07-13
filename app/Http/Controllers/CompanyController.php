4a7878121be2b274fd8a41982ec30e537d8144b2
Lucus0203 <liting68@163.com>
2016-07-13 09:55:06 +0800

    未注册用户

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -32,6 +32,7 @@ class CompanyController extends Controller
         $users=DB::table('pai_user as user')
             ->select('mobile','mobile_code','created','updated')
             ->where('user_name','=',null)
+            ->orWhere('user_name','=','')
             ->orderBy('id','desc')->paginate(20);
         return view('company.failusers',compact('users'));
     }
